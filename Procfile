de41108a3eaa0c189ad8909a329cc8aebdf7e97b
Breno <brenog.santos41@gmail.com>
2021-06-10 12:52:09 -0300

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn calendarzap:app --log-file=-
---
+++ calendarzap.py
@@ -0,0 +1,191 @@
+from flask import Flask, request
+from twilio.twiml import messaging_response
+import boto3
+import os
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+import time
+import threading
+# Twilio credentials and service SID
+account_sid = os.environ['twilio_account_sid']
+auth_token = os.environ['twilio_auth_token']
+client = Client(account_sid, auth_token)
+
+
+s3 = boto3.resource(
+    service_name='s3',
+    region_name=os.environ['REGION_NAME'],
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+)
+#
+#
+app = Flask(__name__)
+
+
+def daily_reminder(receiver, message):
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=message,
+        to=f'whatsapp:{receiver}'
+    )
+    return message
+# daily_reminder()
+
+
+def ler_file_aws():
+    #s3.Bucket('calendar-zap').upload_file(Filename='LOG.txt', Key='log.txt')
+    #s3.Bucket('calendar-zap').download_file(Key='log.txt', Filename='log.txt')
+    #f = open("LOG.txt", "w")
+    # f.write("brenoaDASDADSAsASasASAsASasAS\n")
+    # f.close()
+    #s3.Bucket('calendar-zap').upload_file(Filename='LOG.txt', Key='log.txt')
+    return "recebeu"
+
+
+def check_alrdy_saved(data):
+    s3.Bucket('calendar-zap').download_file(Key='log.txt', Filename='log.txt')
+    with open("log.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.strip("\n") == data:
+                f.close()
+                return 1
+    f.close()
+    #s3.Bucket('calendar-zap').upload_file(Filename='log.txt', Key='log.txt')
+    return 0
+
+#
+
+
+def separate_datas():
+    datas = []
+    num = []
+    i = 0
+    s3.Bucket('calendar-zap').download_file(Key='log.txt', Filename='log.txt')
+    with open("log.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            print(line[line.find("/")+1:line.find("/")+3])
+            datas.append(line)
+        for data in datas:
+
+            num[i] = int(data[data.find("/")+1:data.find("/")+3])
+            if len(num) > 0:
+                j = i
+                while num[j] < num[j-1] and j > 0:
+                    menor = datas[j]
+                    datas[j] = datas[j-1]
+                    datas[j-1] = menor
+                    j = j - 1
+
+            i = i + 1
+            # if(j > 1):
+            #   while int(datas[datas[j].find("/")+1:datas[j].find("/")+3]) <
+            #i = i + 1
+            #datas = organize(datas)
+    f.close()
+    s3.Bucket('calendar-zap').upload_file(Filename='log.txt', Key='log.txt')
+
+
+def write_file(data):
+    f = open("log.txt", "a")
+    f.write(data)
+    f.close()
+    # separate_datas()
+    s3.Bucket('calendar-zap').upload_file(Filename='log.txt', Key='log.txt')
+
+
+def show_datas():
+    word = ''
+    s3.Bucket('calendar-zap').download_file(Key='log.txt', Filename='log.txt')
+    with open('log.txt') as f:
+        line = f.readline()
+        while line:
+            line = f.readline()
+            word += line
+    return word
+
+
+def del_data(word):
+    s3.Bucket('calendar-zap').download_file(Key='log.txt', Filename='log.txt')
+    with open("log.txt", "r") as f:
+        lines = f.readlines()
+        with open("log.txt", "w") as f:
+            for line in lines:
+                if line.strip("\n") != word:
+                    f.write(line)
+    f.close()
+    s3.Bucket('calendar-zap').upload_file(Filename='log.txt', Key='log.txt')
+
+
+def del_all():
+    f = open("log.txt", "w")
+    f.write("." + "\n")
+    f.close()
+    s3.Bucket('calendar-zap').upload_file(Filename='log.txt', Key='log.txt')
+
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'salvar' in incoming_msg or 'save' in incoming_msg:
+        ler_file_aws()
+        if(incoming_msg.find('/') > 1):
+            data = incoming_msg[(incoming_msg.find('/'))-2:incoming_msg.find('/')+3] + \
+                incoming_msg[incoming_msg.find('/')+3:incoming_msg.find('.')]
+            responded = True
+            if(check_alrdy_saved(data)):
+                quote = 'data já foi gravada'
+                msg.body(quote)
+                responded = True
+            else:
+                word = data + "\n"
+                write_file(word)
+                quote = 'salvo!'
+                msg.body(quote)
+                responded = True
+        else:
+            quote = 'formato inválido'
+            msg.body(quote)
+            responded = True
+
+    elif 'datas' in incoming_msg or 'show' in incoming_msg:
+        word = show_datas()
+        if word == 1:
+            quote = 'Nenhuma data foi salva'
+            msg.body(quote)
+            responded = True
+        msg.body(word)
+        responded = True
+    elif 'del' in incoming_msg:
+        word = incoming_msg[(incoming_msg.find('l')+2):(incoming_msg.find('.'))]
+        del_data(word)
+        quote = 'deletado'
+        msg.body(quote)
+        responded = True
+    elif 'exc tudo' in incoming_msg:
+        del_all()
+        quote = 'datas deletadas'
+        msg.body(quote)
+        responded = True
+    elif 'bolsonaro ou lula' in incoming_msg:
+        quote = 'lula'
+        msg.body(quote)
+        responded = True
+    # if 'datas' in incoming_msg:
+        #word = read_file()
+    if not responded:
+        msg.body('comando inválido, digite: help')
+    return str(resp)
+
+
+#
+if __name__ == '__main__':
+    # daily_reminder()
+    #app.debug = True
+    app.run()
