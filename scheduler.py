75a368c3bb91edde1e84670ae5cd529e25dc3609
Breno <brenog.santos41@gmail.com>
2021-06-10 13:17:12 -0300

    init

--- scheduler.py
+++ scheduler.py
@@ -6,11 +6,17 @@ receiver_list = os.environ['adm']
 scheduler = BlockingScheduler()
 
 
-def the_funct():
-    for numb in receiver_list:
-        #print(f"sending message to {numb}")
-        daily_reminder(numb, 'messages')
+# def the_funct():
+#   for numb in receiver_list:
+#print(f"sending message to {numb}")
+#      daily_reminder(numb, 'messages')
 
 
-scheduler.add_job(the_funct, 'cron', minutes=2)
+#scheduler.add_job(the_funct, 'cron', minutes=2)
+
+@scheduler.scheduled_job('interval', minutes=3)
+def timed_job():
+    daily_reminder(os.environ['adm'], 'teste')
+
+
 scheduler.start()
